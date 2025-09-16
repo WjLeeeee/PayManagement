fa0bdb14b81435fb3eaf0f210d6797eb3bbbd30a
woojin-tionnet <dldnwls0115@tionnet.com>
2025-09-16 15:03:46 +0900

    Design - 차트 색상 변경

--- shared/src/commonMain/kotlin/com/woojin/paymanagement/data/ChartData.kt
+++ shared/src/commonMain/kotlin/com/woojin/paymanagement/data/ChartData.kt
@@ -21,23 +21,20 @@ object ChartDataCalculator {
     // 미리 정의된 카테고리별 색상
     private val categoryColors = mapOf(
         // 수입 카테고리
-        "급여" to Color(0xFF2196F3),
-        "식비" to Color(0xFF4CAF50),
-        "당근" to Color(0xFF9C27B0),
-        "K-패스 환급" to Color(0xFF00BCD4),
-        "부수입" to Color(0xFF3F51B5),
-        "투자수익" to Color(0xFF009688),
-        "기타수입" to Color(0xFF607D8B),
-        
+        "급여" to Color(0xFF1E88E5),      // 신뢰감 있는 파란색 (직장/안정감)
+        "식비" to Color(0xFF4CAF50),      // 신선한 초록색 (음식/건강)
+        "당근" to Color(0xFFFF7043),      // 당근색 (당근마켓 브랜드 컬러)
+        "K-패스 환급" to Color(0xFF00ACC1), // 시원한 청록색 (교통/환급)
+        "투자수익" to Color(0xFF388E3C),   // 진한 초록색 (돈/수익)
+        "기타수입" to Color(0xFF9C27B0),   // 보라색 (기타/특별함)
+
         // 지출 카테고리
-        "교통비" to Color(0xFFFF5722),
-        "생활용품" to Color(0xFFE91E63),
-        "쇼핑" to Color(0xFF795548),
-        "의료비" to Color(0xFFFF9800),
-        "교육비" to Color(0xFF8BC34A),
-        "적금" to Color(0xFFFFEB3B),
-        "투자" to Color(0xFF673AB7),
-        "기타지출" to Color(0xFF9E9E9E)
+        "교통비" to Color(0xFF2196F3),     // 파란색 (교통/이동)
+        "생활용품" to Color(0xFF607D8B),   // 회색 (일상용품)
+        "쇼핑" to Color(0xFFE91E63),       // 핑크색 (쇼핑/소비)
+        "적금" to Color(0xFF43A047),       // 초록색 (저축/미래)
+        "투자" to Color(0xFF7B1FA2),       // 진한 보라색 (투자/장기)
+        "기타지출" to Color(0xFF795548)    // 갈색 (기타/일반)
     )
     
     // 기본 색상 팔레트 (카테고리에 정의되지 않은 경우 사용)
