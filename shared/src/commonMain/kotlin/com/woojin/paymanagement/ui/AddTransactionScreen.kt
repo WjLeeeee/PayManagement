fccc8c095330239e65d63c6e8183b686b696ff7a
woojin-tionnet <dldnwls0115@tionnet.com>
2025-09-16 10:06:19 +0900

    Remove - 사용하지 않는 수입 및 지출 내역 제거

--- shared/src/commonMain/kotlin/com/woojin/paymanagement/ui/AddTransactionScreen.kt
+++ shared/src/commonMain/kotlin/com/woojin/paymanagement/ui/AddTransactionScreen.kt
@@ -79,8 +79,8 @@ fun AddTransactionScreen(
     val isEditMode = editTransaction != null
     
     val categories = when (selectedType) {
-        TransactionType.INCOME -> listOf("급여", "식비", "중고거래", "K-패스 환급", "부수입", "투자수익", "기타수입")
-        TransactionType.EXPENSE -> listOf("식비", "교통비", "생활용품", "쇼핑", "의료비", "교육비", "적금", "투자", "기타지출")
+        TransactionType.INCOME -> listOf("급여", "식비", "중고거래", "K-패스 환급", "투자수익", "기타수입")
+        TransactionType.EXPENSE -> listOf("식비", "교통비", "생활용품", "쇼핑", "적금", "투자", "기타지출")
     }
     
     Column(
