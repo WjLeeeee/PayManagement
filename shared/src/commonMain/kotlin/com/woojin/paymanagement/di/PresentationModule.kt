4042125c986c074309e784bff42494c22f325f75
woojin-tionnet <dldnwls0115@tionnet.com>
2025-09-29 14:19:54 +0900

    Revert "Refactor - Main App 파일 리팩토링"
    This reverts commit f9ee5a18de83fdadbdd83079e262a4458d211425.

--- shared/src/commonMain/kotlin/com/woojin/paymanagement/di/PresentationModule.kt
+++ shared/src/commonMain/kotlin/com/woojin/paymanagement/di/PresentationModule.kt
@@ -1,24 +1,16 @@
 package com.woojin.paymanagement.di
 
 import com.woojin.paymanagement.presentation.addtransaction.AddTransactionViewModel
-import com.woojin.paymanagement.presentation.app.AppViewModel
 import com.woojin.paymanagement.presentation.calendar.CalendarViewModel
 import com.woojin.paymanagement.presentation.datedetail.DateDetailViewModel
 import com.woojin.paymanagement.presentation.paydaysetup.PaydaySetupViewModel
 import com.woojin.paymanagement.presentation.statistics.StatisticsViewModel
 import com.woojin.paymanagement.presentation.tutorial.CalendarTutorialViewModel
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.SupervisorJob
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val presentationModule = module {
-    // CoroutineScope for ViewModels
-    single<CoroutineScope> { CoroutineScope(SupervisorJob() + Dispatchers.Main) }
-
     // ViewModels
-    single { AppViewModel(get(), get(), get(), get(), get()) }
     factory { CalendarViewModel(get(), get(), get()) }
     single { AddTransactionViewModel(get(), get(), get(), get(), get()) }
     factory { DateDetailViewModel(get(), get(), get()) }
