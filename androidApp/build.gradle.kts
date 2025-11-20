c5c0ebb113dc0108c3886c81f8f3810b857770d4
woojin-tionnet <dldnwls0115@tionnet.com>
2025-11-20 13:53:11 +0900

    Feat - versionName 및 versionCode 업데이트

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "com.woojin.paymanagement.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.7"
+        versionCode = 12
+        versionName = "1.8"
     }
 
     // 서명 설정
