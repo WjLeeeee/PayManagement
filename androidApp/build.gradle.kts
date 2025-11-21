994dd1f4b77a10897fc267bc9760404834b73380
woojin-tionnet <dldnwls0115@tionnet.com>
2025-11-21 11:16:30 +0900

    Internal - 내부테스트 배포
    1. versionCode 20로 수정

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.woojin.paymanagement.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 19
+        versionCode = 20
         versionName = "1.8"
     }
 
