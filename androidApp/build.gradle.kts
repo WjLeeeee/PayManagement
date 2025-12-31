3488e87ebf3b83012b0edd81cffbab6ff1e7b5f0
Jenkins CI <jenkins@local>
2025-12-31 16:41:18 +0900

    Chore: bump versionCode to 38

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -24,7 +24,7 @@ android {
         applicationId = "com.woojin.paymanagement.android"
         minSdk = 28
         targetSdk = 35
-        versionCode = 37
+        versionCode = 38
         versionName = "1.8"
     }
 
