e301710a49c107ca3f03ad98688469cb1e616883
woojin-tionnet <dldnwls0115@tionnet.com>
2026-09-23 11:19:37 +0900

    Chore: bump versionCode to 80

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.woojin.paymanagement.android"
         minSdk = 26
         targetSdk = 36
-        versionCode = 79
+        versionCode = 80
         versionName = "6.0"
     }
 
